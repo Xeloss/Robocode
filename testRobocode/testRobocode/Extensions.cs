59700b46299e565acda6ac3056c667fd3aa5c2e1
Leandro Galluppi <galluppi.leandro@hotmail.com>
2013-12-02 14:25:56 -0300

    Proyecto para pruebas

---
+++ testRobocode/testRobocode/Extensions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Robocode;
+
+namespace Bot
+{
+    public static class Extensions
+    {
+        public static bool Exists(this object something) 
+        {
+            return something != null;
+        }
+
+        public static bool Is(this ScannedRobotEvent anEvent, EnemyBot aBot)
+        {
+            if (!aBot.Exists())
+                return false;
+
+            return aBot.Name == anEvent.Name;
+        }
+
+        public static bool Is(this RobotDeathEvent anEvent, EnemyBot aBot)
+        {
+            if (!aBot.Exists())
+                return false;
+
+            return aBot.Name == anEvent.Name;
+        }
+
+        public static double GunDeviation(this Robot aRobot)
+        {
+            var gunDeviation = aRobot.GunHeading - aRobot.Heading;
+
+            while (gunDeviation > 180)
+                gunDeviation -= 360;
+
+            while (gunDeviation < -180)
+                gunDeviation += 360;
+
+            return gunDeviation;
+        }
+
+        public static double RadarDeviation(this Robot aRobot)
+        {
+            var radarDeviation = aRobot.RadarHeading - aRobot.Heading;
+
+            while(radarDeviation > 180)
+                radarDeviation -= 360;
+
+            while(radarDeviation < -180)
+                radarDeviation += 360;
+
+            return radarDeviation;
+        }
+    }
+}
---
+++ testRobocode/testRobocode/RamBot.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Drawing;
+using Robocode;
+using Robocode.Util;
+
+namespace Bot
+{
+    public class RamBot : AdvancedRobot
+    {
+        private EnemyBot TargetEnemy { get; set; }
+        private int MoveDirections = 1;
+
+        public override void Run()
+        {
+            this.InitialSetup();
+
+            while (true)
+            {
+                //this.Move();
+                this.AimRadar();
+                this.WaitFor(new RadarTurnCompleteCondition(this));
+                this.WaitFor(new MoveCompleteCondition(this));
+            }
+        }
+
+        public override void OnScannedRobot(ScannedRobotEvent anScannedRobot)
+        {
+            if(this.ShouldUpdateTarget(anScannedRobot))
+                this.TargetEnemy = new EnemyBot(this, anScannedRobot);
+
+            if (anScannedRobot.Is(this.TargetEnemy))
+            {
+                this.AimGun();
+                this.WaitFor(new GunTurnCompleteCondition(this));
+                this.SmartFire();
+            }
+        }
+        public override void OnRobotDeath(RobotDeathEvent aDeadRobot)
+        {
+            if (aDeadRobot.Is(this.TargetEnemy))
+                this.TargetEnemy = null;
+        }
+
+        private void InitialSetup()
+        {
+            this.IsAdjustGunForRobotTurn = true;
+            this.IsAdjustRadarForGunTurn = true;
+            this.IsAdjustRadarForRobotTurn = true;
+
+            this.TargetEnemy = null;
+        }
+
+        private void Move()
+        {
+            if (this.TargetEnemy.Exists())
+            {
+                this.SetTurnRight(this.TargetEnemy.Bearing + 90);
+                this.SetAhead(1000 * MoveDirections);
+            }
+        }
+        private void SmartFire()
+        {
+            var firePower = 400 / this.TargetEnemy.Distance;
+            firePower = Math.Min(firePower, 3);
+            firePower = Math.Max(firePower, 0.1);
+
+            this.Fire(firePower);
+        }
+        private void AimGun()
+        {
+            this.SetTurnGunRight(this.TargetEnemy.BearingFromGun);
+        }
+        private void AimRadar()
+        {
+            if (!this.TargetEnemy.Exists())
+                this.SetTurnRadarRight(360);
+            else
+            {
+                var turningDirection = Math.Sign(this.TargetEnemy.BearingFromRadar);
+                var turningDistance = this.TargetEnemy.BearingFromRadar + turningDirection * 22;
+
+                this.SetTurnRadarRight(turningDistance);
+            }
+        }
+        
+        private bool ShouldUpdateTarget(ScannedRobotEvent anScannedRobot)
+        {
+            if(!this.TargetEnemy.Exists())
+                return true;
+
+            if (anScannedRobot.Is(this.TargetEnemy))
+                return true;
+
+            if (anScannedRobot.Distance < TargetEnemy.Distance)
+                return true;
+
+            return false;
+        }
+    }
+}
